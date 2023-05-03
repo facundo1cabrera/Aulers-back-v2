9bebcc24ec65aabda8ef47ce817f5da64da64f0e
Facundo <cabrera.facundo990@gmail.com>
2023-05-03 17:08:06 -0300

    feat: add Garment model

---
+++ WebApplication1/Models/Garment.cs
@@ -0,0 +1,18 @@
+﻿namespace AulersApi.Models
+{
+    public class Garment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PrimaryUrl { get; set; }
+        public string ModelUrl { get; set; }
+        public char Gender { get; set; }
+        public string Size { get; set; }
+        public bool IsDeleted { get; set; }
+        public decimal Price { get; set; }
+        public DateTime SinceDate { get; set; }
+        public bool Available { get; set; }
+
+    }
+}
